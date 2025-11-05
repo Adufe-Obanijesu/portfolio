0ca9fd92d8f0df1bb14ada4b2eb8169937e68bab
Adufe-Obanijesu <obanijesuadufe8@gmail.com>
2025-11-05 15:54:02 +0100

    chore: show projects

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
-// import Projects from "../components/Projects";
+import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
@@ -51,7 +51,7 @@ function App() {
         <About />
         <Skills />
         <Experience />
-        {/* <Projects /> */}
+        <Projects />
         <Contact />
         <Footer />
       </div>
